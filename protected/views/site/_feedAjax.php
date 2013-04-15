9819afcbda3992ea0d8c478da235b74ef7dc6a74
maxro.developer <maxro.kuzmitch@gmail.com>
2013-04-15 13:43:37 +0500

    Add index.php

--- protected/views/site/_feedAjax.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $this->widget('CFeedBlog', array(
-        'dataProvider'=>$dataProvider,
-        'itemView'=>'_feedItem',
-        'ajaxUpdate'=>false,
-        'template'=>'{items}'
-    ));
-?>
--- protected/views/site/_feedItem.php
+++
@@ -1,6 +0,0 @@
-<article>
-    <h2><?=$data->title;?></h2>
-    <?php
-        echo $data->short_description;
-    ?>
-</article>
